8052bb8bf494266be9010a8b5e14bf3469edc09a
Stanislaw Baranski <stan@stan.bar>
2021-05-30 13:15:25 +0200

    move hashes to constant

--- src/reverse_hash.rs
+++ src/reverse_hash.rs
@@ -1,8 +1,46 @@
 use rayon::prelude::*;
 use std::time::Instant;
 
-const CHARS: &[u8] = b"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#%^-_=+([{<)]}>";
+const CHARS: &[u8] =
+    b"qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#%^-_=+([{<)]}>";
 
+const HASHES: &[(usize, [u8; 16])] = &[
+    (
+        2,
+        [
+            0xBD, 0x84, 0xE6, 0xFB, 0xC0, 0x6A, 0x36, 0x73, 0x5D, 0xBC, 0xBD, 0x54, 0x96, 0x31,
+            0x7A, 0xB2,
+        ],
+    ),
+    (
+        3,
+        [
+            0x4C, 0xC4, 0x06, 0x23, 0x63, 0x55, 0xC4, 0xC3, 0x2E, 0x4D, 0xB5, 0x86, 0x77, 0x84,
+            0x52, 0xA1,
+        ],
+    ),
+    (
+        4,
+        [
+            0x42, 0xD6, 0x05, 0x85, 0x44, 0xD9, 0xC0, 0x0B, 0x32, 0xB7, 0x33, 0x2E, 0x0C, 0xA7,
+            0x95, 0x5A,
+        ],
+    ),
+    (
+        5,
+        [
+            0x01, 0x3E, 0x59, 0x9F, 0xB1, 0x4C, 0x10, 0xBA, 0x0C, 0xCF, 0x23, 0x26, 0x6F, 0x4B,
+            0x4D, 0x0E,
+        ],
+    ),
+    (
+        6,
+        [
+            0x3C, 0x53, 0xFF, 0xDD, 0x7D, 0xBD, 0xF2, 0xA1, 0xE4, 0xD3, 0xC9, 0x8F, 0x55, 0x9E,
+            0xC0, 0x7C,
+        ],
+    ),
+];
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -11,45 +49,9 @@ fn main() {
         None => false,
     };
 
-    vec![
-        (
-            2,
-            [
-                0xBD, 0x84, 0xE6, 0xFB, 0xC0, 0x6A, 0x36, 0x73, 0x5D, 0xBC, 0xBD, 0x54, 0x96, 0x31,
-                0x7A, 0xB2,
-            ],
-        ),
-        (
-            3,
-            [
-                0x4C, 0xC4, 0x06, 0x23, 0x63, 0x55, 0xC4, 0xC3, 0x2E, 0x4D, 0xB5, 0x86, 0x77, 0x84,
-                0x52, 0xA1,
-            ],
-        ),
-        (
-            4,
-            [
-                0x42, 0xD6, 0x05, 0x85, 0x44, 0xD9, 0xC0, 0x0B, 0x32, 0xB7, 0x33, 0x2E, 0x0C, 0xA7,
-                0x95, 0x5A,
-            ],
-        ),
-        (
-            5,
-            [
-                0x01, 0x3E, 0x59, 0x9F, 0xB1, 0x4C, 0x10, 0xBA, 0x0C, 0xCF, 0x23, 0x26, 0x6F, 0x4B,
-                0x4D, 0x0E,
-            ],
-        ),
-        (
-            6,
-            [
-                0x3C, 0x53, 0xFF, 0xDD, 0x7D, 0xBD, 0xF2, 0xA1, 0xE4, 0xD3, 0xC9, 0x8F, 0x55, 0x9E,
-                0xC0, 0x7C,
-            ],
-        ),
-    ]
-    .iter()
-    .for_each(|x| brute_force(reverse_order, x.0, x.1));
+    HASHES
+        .iter()
+        .for_each(|x| brute_force(reverse_order, x.0, x.1));
 }
 
 fn brute_force(reverse_order: bool, chars_count: usize, expected: [u8; 16]) {
@@ -60,13 +62,15 @@ fn brute_force(reverse_order: bool, chars_count: usize, expected: [u8; 16]) {
         chars.reverse();
     }
 
-    let p = permutations(&chars[..], chars_count).par_bridge().find_any(|p| {
-        if expected == whirlpool::core::hash(p.clone()) {
-            true
-        } else {
-            false
-        }
-    });
+    let p = permutations(&chars[..], chars_count)
+        .par_bridge()
+        .find_any(|p| {
+            if expected == whirlpool::core::hash(p.clone()) {
+                true
+            } else {
+                false
+            }
+        });
     match p {
         None => panic!("Did not find any input"),
         Some(x) => {
